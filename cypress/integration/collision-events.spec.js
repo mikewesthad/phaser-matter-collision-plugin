68a07c21fb5b91197d7150befc615f4c634e7024
Michael Hadley <michaelwesthadley@gmail.com>
2018-08-15 06:35:29 -0500

    remove all files from gh-pages

--- cypress/integration/collision-events.spec.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-
-const testName = "collision-events";
-
-context(`Run ${testName}`, () => {
-  it("should run with no errors", () => {
-    cy.runPhaserTest(`/tests/${testName}`);
-  });
-});
--- cypress/integration/gameobject-collide-gameobject.spec.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-
-const testName = "gameobject-collide-gameobject";
-
-context(`Run ${testName}`, () => {
-  it("should run with no errors", () => {
-    cy.runPhaserTest(`/tests/${testName}`);
-  });
-});
