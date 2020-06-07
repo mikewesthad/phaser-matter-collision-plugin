b89198ea158e1868a43cc849c906560f7176c591
Michael Hadley <michaelwesthadley@gmail.com>
2020-06-07 09:07:53 -0500

    Refactor utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,42 +1,18 @@
-import { Physics } from "phaser";
 import logger from "./logger";
 
-// prettier-ignore
-import M = Physics.Matter.Matter;
-
 /**
- * Get the root body of a compound Matter body
- * @private
+ * Get the root body of a compound Matter body.
  */
-export function getRootBody(body: M.Body) {
+export function getRootBody(body: MatterJS.BodyType) {
   while (body.parent !== body) body = body.parent;
   return body;
 }
 
 /**
- * Duck type to check if the given object is a Matter body (because there isn't a prototype)
- * @private
+ * @param obj
  */
-export function isMatterBody(obj: any) {
-  return (
-    obj.hasOwnProperty("collisionFilter") &&
-    obj.hasOwnProperty("parts") &&
-    obj.hasOwnProperty("slop")
-  );
-}
-
-/**
- * Check if object is an acceptable physical object for this plugin - a Matter Body, a tile, or an
- * object with a body property
- * @private
- */
-export function isPhysicsObject(obj: any) {
-  return isMatterBody(obj) || obj.body || obj instanceof Phaser.Tilemaps.Tile;
-}
-
-/** @private */
 export function warnInvalidObject(obj: any) {
   logger.warn(
-    `Expected a Matter body, Tile or an object with a body property, but instead, recieved: ${obj}`
+    `Expected a Matter body, a Tile, a GameObject, a Sprite, an Image, a TileBody, or an object with a body property, but instead, received: ${obj}`
   );
 }
