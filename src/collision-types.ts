7f7bb8f9def5ebdd8ce1b10ada43b9fca5e6636f
Michael Hadley <michaelwesthadley@gmail.com>
2020-06-07 09:27:43 -0500

    Add types for the collisions

---
+++ src/collision-types.ts
@@ -0,0 +1,61 @@
+import { Physics, Types } from "phaser";
+import { CollidingObject } from "./valid-collision-object";
+
+export type MatterCollisionData = Types.Physics.Matter.MatterCollisionData;
+export type ExtendedMatterCollisionData = MatterCollisionData & {
+  gameObjectA?: CollidingObject;
+  gameObjectB?: CollidingObject;
+};
+
+export type CollisionEvent =
+  | Physics.Matter.Events.CollisionStartEvent
+  | Physics.Matter.Events.CollisionActiveEvent
+  | Physics.Matter.Events.CollisionEndEvent;
+
+export type EventData<T extends CollidingObject, K extends CollidingObject> = {
+  bodyA: MatterJS.Body;
+  bodyB: MatterJS.Body;
+  gameObjectA?: T;
+  gameObjectB?: K;
+  pair: ExtendedMatterCollisionData;
+};
+
+export type CollideCallback<T extends CollidingObject, K extends CollidingObject> = (
+  event: EventData<T, K>
+) => void;
+
+export type CollideAConfig<T extends CollidingObject> = {
+  objectA: T | T[];
+  callback: CollideCallback<T, CollidingObject>;
+  context?: CollideContext;
+};
+
+export type CollideABConfig<T extends CollidingObject, K extends CollidingObject> = {
+  objectA: T | T[];
+  objectB: K | K[];
+  callback: CollideCallback<T, K>;
+  context?: CollideContext;
+};
+
+export type CollideConfigUnion =
+  | CollideABConfig<CollidingObject, CollidingObject>
+  | CollideAConfig<CollidingObject>;
+
+export type InternalCollideConfig<T extends CollidingObject, K extends CollidingObject> = {
+  objectA: T | T[];
+  objectB?: K | K[];
+  callback: CollideCallback<T, K>;
+  context?: CollideContext;
+};
+
+export type CollideContext = any;
+
+export type ListenerInfo<T extends CollidingObject, K extends CollidingObject> = {
+  target?: CollidingObject;
+  callback: CollideCallback<T, K>;
+  context?: CollideContext;
+};
+
+export type ListenerMap = Map<CollidingObject, ListenerInfo<any, any>[]>;
+
+export type Unsubscribe = () => void;
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,31 @@ import {
   ObjectWithBody,
   CollidingObject,
 } from "./valid-collision-object";
+import {
+  ExtendedMatterCollisionData,
+  CollisionEvent,
+  EventData,
+  CollideCallback,
+  CollideAConfig,
+  CollideABConfig,
+  CollideContext,
+  Unsubscribe,
+} from "./collision-types";
 
 export default PhaserMatterCollisionPlugin;
 
-export { getRootBody, isCollidingObject, isMatterBody, ObjectWithBody, CollidingObject };
+export {
+  getRootBody,
+  isCollidingObject,
+  isMatterBody,
+  ObjectWithBody,
+  CollidingObject,
+  ExtendedMatterCollisionData,
+  CollisionEvent,
+  EventData,
+  CollideCallback,
+  CollideAConfig,
+  CollideABConfig,
+  CollideContext,
+  Unsubscribe,
+};
