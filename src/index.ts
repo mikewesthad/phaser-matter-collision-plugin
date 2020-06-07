06afdfe5f7944e7183c73d2f96dc65dfa8cf7c7d
Michael Hadley <michaelwesthadley@gmail.com>
2020-06-07 09:08:12 -0500

    Add valid collision objects and utils to export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import PhaserMatterCollisionPlugin from "./phaser-matter-collision-plugin";
+import { getRootBody } from "./utils";
+import {
+  isCollidingObject,
+  isMatterBody,
+  ObjectWithBody,
+  CollidingObject,
+} from "./valid-collision-object";
 
 export default PhaserMatterCollisionPlugin;
+
+export { getRootBody, isCollidingObject, isMatterBody, ObjectWithBody, CollidingObject };
