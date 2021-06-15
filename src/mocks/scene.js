6b11dab43c8a53b1e65a9f3e1482820e5453d5f2
Mike Hadley <michaelwesthadley@gmail.com>
2021-06-15 13:30:27 -0500

    Update scene mock to use Phaser's EventEmitter

--- src/mocks/scene.js
+++ src/mocks/scene.js
@@ -1,11 +1,11 @@
-import EventEmitter from "eventemitter3";
+const EventEmitter = require("phaser/src/events/EventEmitter");
 
 export default class Scene {
   constructor({ addMatter = true } = {}) {
     this.events = new EventEmitter();
     if (addMatter)
       this.matter = {
-        world: new EventEmitter()
+        world: new EventEmitter(),
       };
   }
 }
