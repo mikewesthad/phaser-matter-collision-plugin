cba9b6ddf4455ea3e055b380e5dc023576d45343
Mike Hadley <michaelwesthadley@gmail.com>
2021-06-15 13:31:05 -0500

    Update Phaser mock

--- src/mocks/phaser.js
+++ src/mocks/phaser.js
@@ -1,19 +1,31 @@
-import EventEmitter from "eventemitter3";
 import { MatterTileBody, Tile } from "./game-objects";
+const EventEmitter = require("phaser/src/events/EventEmitter");
+const SceneEvents = require("phaser/src/scene/events/index");
+const MatterEvents = require("phaser/src/physics/matter-js/events/index");
 
-export default {
-  Plugins: {
-    ScenePlugin: class ScenePlugin {}
-  },
-  Physics: {
-    Matter: {
-      TileBody: MatterTileBody
-    }
-  },
-  Tilemaps: {
-    Tile
-  },
-  Events: {
-    EventEmitter
-  }
+const Plugins = {
+  ScenePlugin: class ScenePlugin {},
 };
+const Physics = {
+  Matter: {
+    TileBody: MatterTileBody,
+    Events: MatterEvents,
+  },
+};
+const Tilemaps = { Tile };
+const Events = { EventEmitter };
+const Scenes = {
+  Events: SceneEvents,
+};
+const Types = {};
+const Phaser = {
+  Plugins,
+  Physics,
+  Tilemaps,
+  Events,
+  Scenes,
+  Types,
+};
+
+export default Phaser;
+export { Plugins, Physics, Tilemaps, Events, Types, Scenes };
