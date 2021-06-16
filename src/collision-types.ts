695df4186664a6af27acf7436af3d232867429a9
Mike Hadley <michaelwesthadley@gmail.com>
2021-06-16 14:39:36 -0500

    Add shorthand "CO" for CollidingObject in types

--- src/collision-types.ts
+++ src/collision-types.ts
@@ -1,13 +1,14 @@
 import { Physics, Types } from "phaser";
 import { CollidingObject } from "./valid-collision-object";
 
+type CO = CollidingObject;
 /** Alias to Matter type. */
 export type MatterCollisionData = Types.Physics.Matter.MatterCollisionData;
 
 /** Extended matter collision event data with game objects. */
 export interface ExtendedMatterCollisionData extends MatterCollisionData {
-  gameObjectA?: CollidingObject;
-  gameObjectB?: CollidingObject;
+  gameObjectA?: CO;
+  gameObjectB?: CO;
 }
 
 export type CollisionEvent =
@@ -15,7 +16,7 @@ export type CollisionEvent =
   | Physics.Matter.Events.CollisionActiveEvent
   | Physics.Matter.Events.CollisionEndEvent;
 
-export interface EventData<T extends CollidingObject, K extends CollidingObject> {
+export interface EventData<T extends CO, K extends CO> {
   bodyA: MatterJS.Body;
   bodyB: MatterJS.Body;
   gameObjectA?: T;
@@ -24,19 +25,19 @@ export interface EventData<T extends CollidingObject, K extends CollidingObject>
   pair: ExtendedMatterCollisionData;
 }
 
-export interface CollideCallback<T extends CollidingObject, K extends CollidingObject> {
+export interface CollideCallback<T extends CO, K extends CO> {
   (event: EventData<T, K>): void;
 }
 
 /** Config for specified A object(s) vs anything else collision listeners. */
-export interface CollideAConfig<T extends CollidingObject> {
+export interface CollideAConfig<T extends CO> {
   objectA: T | T[];
-  callback: CollideCallback<T, CollidingObject>;
+  callback: CollideCallback<T, CO>;
   context?: CollideContext;
 }
 
 /** Config for specified A object(s) vs specified B object(s). */
-export interface CollideABConfig<T extends CollidingObject, K extends CollidingObject> {
+export interface CollideABConfig<T extends CO, K extends CO> {
   objectA: T | T[];
   objectB: K | K[];
   callback: CollideCallback<T, K>;
@@ -44,9 +45,9 @@ export interface CollideABConfig<T extends CollidingObject, K extends CollidingO
 }
 
 export interface InternalCollideConfig {
-  objectA: CollidingObject | CollidingObject[];
-  objectB?: CollidingObject | CollidingObject[];
-  callback: CollideCallback<CollidingObject, CollidingObject>;
+  objectA: CO | CO[];
+  objectB?: CO | CO[];
+  callback: CollideCallback<CO, CO>;
   context?: CollideContext;
 }
 
@@ -58,7 +59,7 @@ export interface ListenerInfo<T extends CollidingObject, K extends CollidingObje
   context?: CollideContext;
 }
 
-export type ListenerMap = Map<CollidingObject, ListenerInfo<any, any>[]>;
+export type ListenerMap = Map<CO, ListenerInfo<any, any>[]>;
 
 export interface Unsubscribe {
   (): void;
