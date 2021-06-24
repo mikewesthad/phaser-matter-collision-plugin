ed0466671851cd418ded50bc62b9738e6cd40be4
Mike Hadley <michaelwesthadley@gmail.com>
2021-06-24 17:05:21 -0500

    Add a typescript example in a workspace

---
+++ workspaces/typescript-example/src/main-scene.ts
@@ -0,0 +1,141 @@
+import Phaser, { Physics, Scene, Cameras, Tilemaps, Types, Input, GameObjects } from "phaser";
+import { EventData } from "../../phaser-matter-collision-plugin/dist";
+//import { CollideABConfig, EventData } from "phaser-matter-collision-plugin";
+//import MSprite = Physics.Matter.Sprite;
+
+export default class MainScene extends Scene {
+  private controls: Cameras.Controls.FixedKeyControl;
+
+  preload() {
+    this.load.tilemapTiledJSON("map", "../assets/tilemaps/level.json");
+    this.load.image(
+      "kenney-tileset-64px-extruded",
+      "../assets/tilesets/kenney-tileset-64px-extruded.png"
+    );
+    this.load.atlas("emoji", "../assets/atlases/emoji.png", "../assets/atlases/emoji.json");
+  }
+
+  create() {
+    const map = this.make.tilemap({ key: "map" });
+    const tileset = map.addTilesetImage("kenney-tileset-64px-extruded");
+    const groundLayer = map.createLayer("Ground", tileset, 0, 0);
+    const lavaLayer = map.createLayer("Lava", tileset, 0, 0);
+
+    // Set colliding tiles *before* converting the layer to Matter bodies.
+    groundLayer.setCollisionByProperty({ collides: true });
+    lavaLayer.setCollisionByProperty({ collides: true });
+    this.matter.world.convertTilemapLayer(groundLayer);
+    this.matter.world.convertTilemapLayer(lavaLayer);
+
+    // Set up the bounds.
+    this.matter.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+    // Create two simple animations - one angry => grimace emoji and one heart eyes => grimace
+    this.anims.create({
+      key: "angry",
+      frames: [
+        { key: "emoji", frame: "1f92c" },
+        { key: "emoji", frame: "1f62c" },
+      ],
+      frameRate: 3,
+      repeat: 0,
+    });
+    this.anims.create({
+      key: "love",
+      frames: [
+        { key: "emoji", frame: "1f60d" },
+        { key: "emoji", frame: "1f62c" },
+      ],
+      frameRate: 3,
+      repeat: 0,
+    });
+
+    // Create some Matter-enabled physics game objects.
+    const bodyOptions = { restitution: 1, friction: 0, shape: "circle" };
+    const emoji1 = this.matter.add.sprite(350, 100, "emoji", "1f62c", bodyOptions);
+    const emoji2 = this.matter.add.sprite(350, 275, "emoji", "1f62c", bodyOptions);
+    const emoji3 = this.matter.add.image(350, 350, "emoji", "1f4a9", bodyOptions);
+
+    // Only listen for collisions between emoji 1 & 2 - make them love-hate when they start
+    // colliding or continue colliding
+    this.matterCollision.addOnCollideStart({
+      objectA: emoji1,
+      objectB: emoji2,
+      callback: ({ gameObjectA, gameObjectB }) => {
+        // These fields *can* be undefined depending on the inputs, so use the non-null assertion
+        // operator when we know more than the type system.
+        gameObjectA!.play("angry", false); // gameObjectA will always match the given "objectA"
+        gameObjectB!.play("love", false); // gameObjectB will always match the given "objectB"
+      },
+    });
+
+    this.matterCollision.addOnCollideActive({
+      objectA: emoji1,
+      objectB: emoji2,
+      callback: ({ gameObjectA, gameObjectB }) => {
+        gameObjectA!.play("angry", false);
+        gameObjectB!.play("love", false);
+      },
+    });
+
+    // Kill poop emoji on collision with lava.
+    const unsubscribe = this.matterCollision.addOnCollideStart({
+      objectA: emoji3,
+      callback: ({ gameObjectB }) => {
+        if (!gameObjectB || !(gameObjectB instanceof Phaser.Tilemaps.Tile)) return;
+        if (gameObjectB.index === 290) {
+          emoji3.destroy();
+          unsubscribe();
+        }
+      },
+    });
+
+    // Make the emoji draggable
+    emoji1.setInteractive();
+    emoji2.setInteractive();
+    emoji3.setInteractive();
+    this.input.setDraggable(emoji1);
+    this.input.setDraggable(emoji2);
+    this.input.setDraggable(emoji3);
+    this.input.on(
+      "drag",
+      (pointer: Input.Pointer, gameObject: Physics.Matter.Sprite, x: number, y: number) =>
+        gameObject.setPosition(x, y)
+    );
+    this.input.on("dragstart", (pointer: Input.Pointer, gameObject: Physics.Matter.Sprite) =>
+      gameObject.setStatic(true)
+    );
+    this.input.on("dragend", (pointer: Input.Pointer, gameObject: Physics.Matter.Sprite) =>
+      gameObject.setStatic(false)
+    );
+
+    Phaser.Input.Events.DRAG;
+    const text = "Click and drag the emoji.\nArrow keys to move the camera.";
+    const help = this.add.text(16, 16, text, {
+      fontSize: "18px",
+      padding: { x: 10, y: 5 },
+      backgroundColor: "#ffffff",
+      color: "#000000",
+    });
+    help.setScrollFactor(0).setDepth(1000);
+
+    const cursors = this.input.keyboard.createCursorKeys();
+    const controlConfig = {
+      camera: this.cameras.main,
+      left: cursors.left,
+      right: cursors.right,
+      up: cursors.up,
+      down: cursors.down,
+      speed: 0.5,
+    };
+    this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
+
+    this.cameras.main.scrollX = 100;
+    this.cameras.main.scrollY = 100;
+  }
+
+  update(time: number, delta: number) {
+    this.controls.update(delta);
+  }
+}
